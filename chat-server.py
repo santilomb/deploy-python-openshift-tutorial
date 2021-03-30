1aa5aebbebd305fbdc473eed2052a46c2c1b7cad
Santiago Lombardi <santiago.lombardi@gmail.com>
2021-03-30 09:37:18 -0300

    Update chat-server.py

--- chat-server.py
+++ chat-server.py
@@ -1,17 +1,20 @@
 # -*- coding: UTF-8 -*-
 
 from flask import Flask, redirect, url_for, request 
+import os.path
 app = Flask(__name__)   
 
 @app.route('/', methods=["GET", "POST"]) 
 def main():
+    if not os.path.exists("chat_lines.txt"):
+        os.touch("chat_lines.txt")
     if request.method == 'POST':
         mensaje = request.form['mensaje']
-        with open('/tmp/chat.txt', 'a') as file:
+        with open("chat_lines.txt", 'w+') as file:
             file.write(mensaje)
         return "ok"
     else:
-        with open('/tmp/chat.txt') as file:
+        with open("chat_lines.txt") as file:
             lines = file.read()
         return lines
 
